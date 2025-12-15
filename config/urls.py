3b70c516a3b92fa7bc6024ded9be398b01429324
Joel Alcocer <Jhostinjoel@gmail.com>
2025-12-15 14:56:25 -0500

    Fix: Enable media serving in production for Render

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +30,7 @@ urlpatterns = [
     path('inventory/', include('apps.inventory.urls')),
     path('sales/', include('apps.sales.urls')),
     path('chatbot/', include('apps.chatbot.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
