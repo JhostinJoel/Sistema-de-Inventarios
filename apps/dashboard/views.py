9b76823917050cf66c17b2b7434afac26a73ac0a
Joel Alcocer <Jhostinjoel@gmail.com>
2025-12-15 15:06:06 -0500

    Fix: Display total users count on dashboard

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -6,16 +6,21 @@ from django.db.models import Sum, Count
 from django.utils import timezone
 from datetime import timedelta
 
+from django.contrib.auth import get_user_model
+
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
+        User = get_user_model()
+        
         # General Stats
         context['total_products'] = Product.objects.count()
         context['low_stock_products'] = Product.objects.filter(stock__lte=5).count()
         context['total_sales_today'] = Sale.objects.filter(date__date=timezone.now().date()).count()
+        context['total_users'] = User.objects.count()
         
         # Recent Activity
         context['recent_sales'] = Sale.objects.order_by('-date')[:5]
